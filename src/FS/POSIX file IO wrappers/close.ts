c0d500da5ae881dbff23a6c98f42c60de76b20ce
Sabry Awad <dr.sabry1997@gmail.com>
2022-09-14 18:55:04 +0200

    Closing Files

---
+++ src/FS/POSIX file IO wrappers/close.ts
@@ -0,0 +1,15 @@
+import fs from 'fs/promises';
+import { close } from 'fs';
+import { promisify } from 'util';
+
+const path = __dirname + '/data/foo.txt';
+
+const _close = promisify(close);
+
+try {
+  const fh = await fs.open(path, 'a');
+  await fs.appendFile(fh, 'data to append', 'utf8');
+  await _close(fh.fd);
+} catch (error) {
+  console.error(error);
+}
