c52f6c6601cba6aa1ebe804e3f8ff5e8ffa553c3
Sabry Awad <dr.sabry1997@gmail.com>
2022-09-14 16:36:42 +0200

    stdin ReadStream

---
+++ src/Fundamentals/stdin.ts
@@ -0,0 +1,6 @@
+// stdin
+// receives data from the terminal window
+// accept input from the user at runtime
+// can receive its data from a file or another program.
+
+console.log('process.stdin: ' + process.stdin.constructor.name);
