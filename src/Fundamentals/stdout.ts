f9922bed4dce5b7df47f45d1d9d0c59deb5c984b
Sabry Awad <dr.sabry1997@gmail.com>
2022-09-14 16:55:46 +0200

    echo the text

--- src/Fundamentals/stdout.ts
+++ src/Fundamentals/stdout.ts
@@ -1,6 +1,26 @@
 // writable stream to which programs should direct their output.
 
 // direct data (buffer or string) to terminal
-process.stdout.write('data', 'utf8', () => {
-  // executed once the data is successfully written to stdout.
+// process.stdout.write('data', 'utf8', () => {
+//   // executed once the data is successfully written to stdout.
+// });
+
+const { stdin, stdout } = process;
+
+// Encoding for stream
+stdin.setEncoding('utf8');
+
+// readable event emitted when a chunk of data is ready to be read
+stdin.on('readable', function () {
+  // read the chunk in
+  const input: string = stdin.read();
+
+  if (input !== null) {
+    // echo the text
+    stdout.write(input.toUpperCase());
+
+    stdin.resume();
+
+    input.trim() === 'EXIT' && process.exit(0); // exit with success
+  }
 });
