5b948938e32c41f6501d1237659b8fbd9f3952ca
Sabry Awad <dr.sabry1997@gmail.com>
2022-09-14 18:56:04 +0200

    fh close

--- src/FS/POSIX file IO wrappers/close.ts
+++ src/FS/POSIX file IO wrappers/close.ts
@@ -10,6 +10,7 @@ try {
   const fh = await fs.open(path, 'a');
   await fs.appendFile(fh, 'data to append', 'utf8');
   await _close(fh.fd);
+  // await fh.close();
 } catch (error) {
   console.error(error);
 }
