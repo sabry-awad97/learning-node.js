cbae5c8765b56d3b87d266ec44b3dff1a3bfa22c
Sabry Awad <dr.sabry1997@gmail.com>
2022-09-14 17:12:44 +0200

    console error under the hood

--- src/Fundamentals/srderr.ts
+++ src/Fundamentals/srderr.ts
@@ -2,6 +2,27 @@
 // used for displaying error and warning messages
 
 // direct data (buffer or string) to terminal
-process.stderr.write('data', 'utf8', () => {
-  // executed once the data is successfully written to stdout.
-});
+// process.stderr.write('data', 'utf8', () => {
+//   // executed once the data is successfully written to stdout.
+// });
+
+import { format } from 'util';
+
+class Logger {
+  constructor(private stderr: NodeJS.WriteStream) {}
+
+  warn(...args: any[]) {
+    this.stderr.write(format(...args) + '\n');
+  }
+
+  error(...args: any[]) {
+    this.warn(...args);
+  }
+}
+
+const logger = new Logger(process.stderr);
+
+logger.warn('I love you');
+
+// console.error();
+// console.warn();
