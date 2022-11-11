f86777f8e3a251d4e7ee985c1caf2aaca3b1adfe
Sabry Awad <dr.sabry1997@gmail.com>
2022-11-11 08:53:57 +0200

    basic emitter

--- src/Async Programming/events/events.ts
+++ src/Async Programming/events/events.ts
@@ -1,18 +1,26 @@
 import { EventEmitter } from 'events';
 
+let counter = 1;
+
+// Instantiate an EventEmitter object - used to generate new events -
 const emitter = new EventEmitter();
 
-emitter.on('customEvent', (message, user) => {
-  console.log(`${user}: ${message}`);
+// 1- attach an event handler (listener) to an event
+// return the EventEmitter instance to allow chaining
+emitter.on('connection', data => {
+  // used to trap the event and process it
+
+  console.log('connection lisetner is executed ' + data);
 });
 
-process.stdin.on('data', data => {
-  const input = data.toString().trim();
-  if (input === 'exit') {
-    emitter.emit('customEvent', 'Goodbye!', 'process');
-    process.exit();
-  }
-  emitter.emit('customEvent', input, 'terminal');
-});
+// emitter.addListener("connection", data => {
+//   console.log("connection listener is executed " + data);
+// });
 
-// console.log(Buffer.from('1b5b304b', 'hex').toString());
+// 2- produce the actual event
+// Event names are defined by calling .emit with the event name
+// return the EventEmitter instance to allow chaining
+emitter.emit('connection', counter++);
+emitter.emit('connection', counter++);
+
+// emitter.emit("error", new Error("Mistake"))
